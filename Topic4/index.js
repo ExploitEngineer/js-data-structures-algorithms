3cba033d060501fd5bdbd99a6efad87f7527f51d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-05-26 18:47:36 +0500

    feat: add pattern programming triangle pattern

--- Topic4/index.js
+++ Topic4/index.js
@@ -115,3 +115,16 @@ for (let i = 1; i <= 5; i++) {
   }
   console.log();
 }
+
+// triangle pattern
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    process.stdout.write(" ");
+  }
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    process.stdout.write("*");
+  }
+  console.log();
+}
