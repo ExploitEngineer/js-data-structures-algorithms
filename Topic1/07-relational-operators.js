395d593026abbf5f4d23d3ee5cfa11994d36b292
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-08 03:18:58 +0500

    feat(topic-1): add relational operators

---
+++ Topic1/07-relational-operators.js
@@ -0,0 +1,15 @@
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 >= 5);
+console.log(10 <= 7);
+console.log(10 != 5);
+console.log(10 !== 5);
+
+// =
+let a = 12; // assignment operator
+
+// ==
+console.log(a == "12"); // true
+
+// ===
+console.log(a === "12"); // false
