84fa2bd52d5a63133abaa22b6be1379e141eea03
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-07 23:29:13 +0500

    feat(topic-1): type coercion

---
+++ Topic1/03-type-coercion.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 20;
+
+console.log("sum of 10 and 20 is " + (a + b));
+console.log(a + b + " is sum of 10 and 20");
+
+console.log("1" - 1);
+console.log("1" * 1);
+console.log("1" / 1);
