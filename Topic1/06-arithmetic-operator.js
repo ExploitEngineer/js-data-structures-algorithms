ffe7992b033f256a4d888ba38913943e9d9758de
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-08 03:04:07 +0500

    feat(topic-1): add arithmetic operators

---
+++ Topic1/06-arithmetic-operator.js
@@ -0,0 +1,18 @@
+// Arithmetic Operator
+
+let a = 12; // divident
+let b = 22; // divisor
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b); // quotient
+console.log(a % b); // remainder
+
+let c = 4563;
+
+console.log(c % 10);
+console.log(c % 100);
+console.log(c % 1000);
+
+console.log(Math.floor(c / 10));
